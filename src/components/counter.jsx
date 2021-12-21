6b9351d56592437a52feadf535ca4f004a5caf3c
Md Maksud Hossain <maksud.1969@gmail.com>
2021-12-21 15:39:49 +0600

    React.Fragment.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 class Counter extends React.Component {
     render() { 
         return (
-                <div>
+                <React.Fragment>
                     <h1>Hello World</h1>
                     <button>Increment</button>
-                </div>
+                </React.Fragment>
                 );
     }
 }
