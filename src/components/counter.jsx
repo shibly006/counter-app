85e06bf3062b74f302b100fa439548c4aed12ad0
Md Maksud Hossain <maksud.1969@gmail.com>
2021-12-21 17:19:59 +0600

    inline style

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -14,6 +14,7 @@ class Counter extends React.Component {
         return (
                 <React.Fragment>
                     
+                    {/* <span style={{fontSize:30}} className="badge badge-primary m-2">{this.formatCount()}</span> */}
                     <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
                     <button className="btn btn-secondary btn-sm">Increment</button>
                 </React.Fragment>
