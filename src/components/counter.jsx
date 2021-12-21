9ed5fcace47987e7072971e3f34e3826821adeac
Md Maksud Hossain <maksud.1969@gmail.com>
2021-12-21 19:42:03 +0600

    dynami classess.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,22 +2,22 @@ import React, { Component } from 'react';
 
 class Counter extends React.Component {
     state = {
-        count : 0,
-        imageUrl: 'https://picsum.photos/200'
+        count : 1       
     };
 
-    styles = {
-        fontSize: 10,
-        fontWeight: 'bold'
-    }
-    render() { 
+    render() {         
         return (
-                <React.Fragment>
-                    
-                    <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <React.Fragment>                    
+                    <span className={this.getBadgeClsses()}>{this.formatCount()}</span>
                     <button className="btn btn-secondary btn-sm">Increment</button>
                 </React.Fragment>
-                );
+                );        
+    }
+
+    getBadgeClsses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count == 0 )?  "warning" : "primary";
+        return classes;
     }
 
     formatCount(){
