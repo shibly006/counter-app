c58afb46bfd97418fbebeb802082e2f9fc43899f
Md Maksud Hossain <maksud.1969@gmail.com>
2021-12-21 20:21:36 +0600

    bind in constractor

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,12 +5,22 @@ class Counter extends React.Component {
         count : 1,
         tags: ["tag1", "tag2", "tag3"]       
     };
+    
+    constructor(){
+        super();
+        console.log("Constructor", this);
+        this.handleIncrement= this.handleIncrement.bind(this);
+    }
+
+    handleIncrement(){
+        console.log('Increment Clicked', this);
+    }
 
     render() {         
         return (
                 <React.Fragment>                    
                     <span className={this.getBadgeClsses()}>{this.formatCount()}</span>
-                    <button className="btn btn-secondary btn-sm">Increment</button>
+                    <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
                     <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>                    
                 </React.Fragment>
                 );        
