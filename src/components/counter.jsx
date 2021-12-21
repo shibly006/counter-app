767dbe7cc16c4c2050de018e223f402c4ae41834
Md Maksud Hossain <maksud.1969@gmail.com>
2021-12-21 15:54:20 +0600

    state added.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 
 class Counter extends React.Component {
+    state = {
+        count : 1
+    };
     render() { 
         return (
                 <React.Fragment>
-                    <h1>Hello World</h1>
+                    <span>{this.state.count}</span>
                     <button>Increment</button>
                 </React.Fragment>
                 );
+    }
+
+    formatCount(){
+        const {count} = this.state;
+        return count == 0 ? 'Zero' : count; 
     }
 }
  
